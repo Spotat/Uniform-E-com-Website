12474280c2beb08bda07a728aa5bf1fda26a5850
Spotat <ngashj88@gmail.com>
2026-06-01 17:12:44 +0300

    first commit

---
+++ static/scripts/navigation.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("sideNav").style.width = "80%"
+    document.getElementById("sideNav").style.display = "block"
+    document.getElementById("sideNav").style.visibility = "visible"
+}
+function closeNav(){
+    document.getElementById("sideNav").style.width = "0%"
+    document.getElementById("sideNav").style.display = "none"
+    document.getElementById("sideNav").style.visibility = "hidden"
+}
