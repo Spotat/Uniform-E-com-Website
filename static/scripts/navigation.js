14a61aa9a4ad7bd527b0354a9682913bbfe9a6cf
Spotat <ngashj88@gmail.com>
2026-06-03 15:55:18 +0300

    navigation sidenav update

--- static/scripts/navigation.js
+++ static/scripts/navigation.js
@@ -1,5 +1,5 @@
 function openNav(){
-    document.getElementById("sideNav").style.width = "80%"
+    document.getElementById("sideNav").style.width = "100%"
     document.getElementById("sideNav").style.display = "block"
     document.getElementById("sideNav").style.visibility = "visible"
 }
